50b8db07bb6cfd0eaffce3c0d72af7df3043693a
Ruslan (MacBook Pro) <rshashkov@icloud.com>
2014-01-12 18:54:17 +0400

    x

--- routes/common.js
+++ routes/common.js
@@ -3,7 +3,7 @@ module.exports = function(app, controllers){
      * Get routes
      * */
     app.get('/', app.ensureAuthenticated, function(req, res){
-        res.render('index', { user: req.user });
+        res.render('main', { user: req.user });
     });
 
     app.get('/account', app.ensureAuthenticated, function(req, res){
