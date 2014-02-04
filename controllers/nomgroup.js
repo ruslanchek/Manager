312940104c67243636e4a84d63ea50f5c0d1b5fb
Ruslan (MacBook Pro) <rshashkov@icloud.com>
2014-02-05 02:22:27 +0400

    x

--- controllers/nomgroup.js
+++ controllers/nomgroup.js
@@ -1,3 +1,31 @@
 module.exports = function (app, models) {
-    return this;
+    this.findItems = function (user, done) {
+        models.nomgroup.find({ _user_id: user._id }, function (err, data) {
+            if (err) {
+                app.log.error('findOne error', err);
+                return done(err);
+            }
+
+            if (data) {
+                return done(false, data);
+            }
+
+            return done(false, false);
+        });
+    };
+
+    this.findOne = function (user, id, done) {
+        models.nomgroup.find({ _user_id: user._id, _id: id }, function (err, data) {
+            if (err) {
+                app.log.error('findOne error', err);
+                return done(err);
+            }
+
+            if (data[0]) {
+                return done(false, data[0]);
+            }else{
+                return done(true);
+            }
+        });
+    };
 };
--- routes/nomenclature.js
+++ routes/nomenclature.js
@@ -4,116 +4,57 @@ module.exports = function(app, controllers){
         section: 'nomenclature'
     };
 
+    var _this = this;
+
+    this.getNomgroups = function(user, params, done){
+        controllers.nomgroup.findItems(user, function(err, data){
+            params.err = err;
+            params.nomgroups = data;
+
+            done(params);
+        });
+    };
+
+    this.getNomenclature = function(user, params, done){
+        controllers.nomenclature.findItems(user, function(err, data){
+            params.data = data;
+            params.err = err;
+
+            _this.getNomgroups(user, params, function(params){
+                done(params);
+            });
+        });
+    };
+
     app.get('/nomenclature', app.ensureAuthenticated, function(req, res){
-        controllers.nomenclature.findItems(req.user, function(err, data){
+        var params = app.utils.extend(common, {
+            user: req.user,
+            nomgroup: false,
+            metadata: {
+                title: 'Номенклатура'
+            }
+        });
+
+        _this.getNomenclature(req.user, params, function(params){
+            res.render('nomenclature', params);
+        });
+    });
+
+    app.get('/nomenclature/:nomgroup', app.ensureAuthenticated, function(req, res){
+        controllers.nomgroup.findOne(req.user, req.params.nomgroup, function(err, data){
             var params = app.utils.extend(common, {
                 err: err,
                 data: data,
                 user: req.user,
+                nomgroup: data,
                 metadata: {
                     title: 'Номенклатура'
                 }
             });
 
-            res.render('nomenclature', params);
-        });
-    });
-
-    app.get('/nomenclature/add', app.ensureAuthenticated, function(req, res){
-        controllers.nomenclature.countAll(req.user, function(data){
-            var params = app.utils.extend(common, {
-                user: req.user,
-                number: (data >= 0) ? data+1 : '',
-                metadata: {
-                    title: 'Новый счет'
-                }
+            _this.getNomenclature(req.user, params, function(params){
+                res.render('nomenclature', params);
             });
-
-            res.render('accounts.add.jade', params);
-        });
-    });
-
-    app.post('/nomenclature/delete', app.ensureAuthenticated, function(req, res){
-        controllers.nomenclature.deleteItems(req.user, req.body.ids, function(result){
-            res.json(result);
-        });
-    });
-
-    app.get('/nomenclature/edit/:id', app.ensureAuthenticated, function(req, res){
-        controllers.nomenclature.findOne(req.user, req.params.id, function(err, data){
-            if(err === true){
-                res.redirect('/404');
-            }else{
-                var params = app.utils.extend(common, {
-                    err : err,
-                    data: data,
-                    user: req.user,
-                    metadata: {
-                        title: 'Редактирование счета'
-                    }
-                });
-
-                res.render('accounts.edit.jade', params);
-            }
-        });
-    });
-
-    app.get('/nomenclature/view/:id', app.ensureAuthenticated, function(req, res){
-        controllers.nomenclature.findOne(req.user, req.params.id, function(err, data){
-            if(err === true){
-                res.redirect('/404');
-            }else{
-                var params = app.utils.extend(common, {
-                    err : err,
-                    data: data,
-                    user: req.user,
-                    metadata: {
-                        title: 'Просмотр счета'
-                    }
-                });
-
-                res.render('accounts.view.jade', params);
-            }
-        });
-    });
-
-    app.get('/nomenclature/pdf/:id', app.ensureAuthenticated, function(req, res){
-        controllers.nomenclature.findOne(req.user, req.params.id, function(err, data){
-            if(err === true){
-                res.redirect('/404');
-            }else{
-                var url = app.config.get('protocol') + '://' + app.config.get('host') + ':' + app.config.get('port') + '/accounts/view/' + req.params.id,
-                    name = 'Счет №' + data.number + ' от ' + app.utils.humanizeDate(data.date);
-
-                app.utils.generatePDF(url, req.cookies['connect.sid'], res, name);
-            }
-        });
-    });
-
-    app.post('/nomenclature/view', app.ensureAuthenticated, function(req, res){
-        var params = app.utils.extend(common, {
-            data: req.body,
-            user: req.user,
-            metadata: {
-                title: 'Просмотр счета'
-            }
-        });
-
-        params.data.date = app.utils.parseDate(params.data.date);
-        params.data.items = JSON.parse(decodeURIComponent(params.data.items));
-
-        res.render('accounts.view.jade', params);
-    });
-
-    app.post('/nomenclature/add', app.ensureAuthenticated, function(req, res){
-        controllers.nomenclature.addItem(req.user, req.body, function(result){
-            res.json(result);
-        });
-    });
-
-    app.post('/nomenclature/edit/:id', app.ensureAuthenticated, function(req, res){
-        controllers.nomenclature.editItem(req.user, req.params.id, req.body, function(result){
-            res.json(result);
         });
     });
 };
